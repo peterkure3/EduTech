c716aedae06e0ea8fb7ac272414f410f3d845bee
Peter Kure <peterkure256@gmail.com>
2023-10-20 11:11:44 +0300

    Back4app config done

--- frontend/App.js
+++ frontend/App.js
@@ -1,20 +1,65 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { View, Button, Text, SafeAreaView } from 'react-native';
 
-export default function App() {
+// In a React Native application
+import Parse from 'parse/react-native.js';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+
+
+//Initializing the SDK
+Parse.setAsyncStorage(AsyncStorage);
+//Paste below the Back4App Application ID AND the JavaScript KEY
+Parse.initialize('86LoMs38gTacRyWXbQGGixJoxtFIr4EOfAb6Ploq', '4EFDQLclVUaWyETFmHBGxaAD0F019m3FGOrJO8l7');
+//Point to Back4App Parse API address 
+Parse.serverURL = 'https://parseapi.back4app.com/';
+
+const App = () => {
+  const [person, setPerson] = useState(new Parse.Object('Person'));
+
+  async function addPerson() {
+    try {
+      //create a new Parse Object instance
+      const newPerson = new Parse.Object('Person');
+      //define the attributes you want for your Object
+      newPerson.set('name', 'John');
+      newPerson.set('email', 'john@back4app.com');
+      //save it on Back4App Data Store
+      await newPerson.save();
+    } catch (error) {
+      console.log('Error saving new person: ', error);
+    }
+  }
+
+  async function fetchPerson() {
+    //create your Parse Query using the Person Class you've created
+    let query = new Parse.Query('Person');
+    //run the query to retrieve all objects on Person class, optionally you can add your filters
+    let queryResult = await query.find();
+    //the resul is an arry of objects. Pick the first result 
+    const currentPerson = queryResult[0];
+    //access the Parse Object attributes
+    console.log('person id: ', currentPerson.get('id'));
+    console.log('person name: ', currentPerson.get('name'));
+    console.log('person email: ', currentPerson.get('email'));
+    setPerson(currentPerson);
+  }
+
+  useEffect(() => {
+    fetchPerson()
+  }, []);
+
   return (
-    <View style={styles.container}>
-      <Text>Hello World</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    <SafeAreaView>
+      <View>
+        <Text>Name: {person.get('name')}</Text>
+        <Text>email: {person.get('email')}</Text>
+        <Button title='Add person' onPress={addPerson} />
+        <Button title='Fetch person' onPress={fetchPerson} />
+        {/* Your other components here ....*/}
+      </View>
+    </SafeAreaView>
+  )
+
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App;
