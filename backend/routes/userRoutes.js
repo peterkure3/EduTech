cc89d1652b2bf6d365be0cb1d2aabb7da82151ae
ottojoash <34677630+ottojoash@users.noreply.github.com>
2023-10-23 08:46:42 +0300

    JSON file

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,6 +1,10 @@
 const express = require('express');
+const bodyParser = require('body-parser'); // Require body-parser
 const router = express.Router();
 const userController = require('../controllers/userController');
+
+// Middleware for JSON parsing using body-parser
+router.use(bodyParser.json());
 
 // Register a new user
 router.post('/register', userController.registerUser);
