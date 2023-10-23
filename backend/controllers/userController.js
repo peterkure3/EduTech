8901dcc605f7ae607e934040317a5d131889de30
ottojoash <34677630+ottojoash@users.noreply.github.com>
2023-10-23 09:16:03 +0300

    user

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -3,6 +3,15 @@ const userModel = require('../models/userModel');
 async function registerUser(req, res) {
   try {
     const { username, email, password } = req.body;
+
+    // Input validation
+    if (!username || !email || !password) {
+      return res.status(400).json({ error: 'Missing username, email, or password' });
+    }
+
+    // You can add additional validation checks here if needed
+
+    // Create the user if input is valid
     const user = await userModel.createUser({ username, email, password });
     res.status(201).json(user);
   } catch (error) {
