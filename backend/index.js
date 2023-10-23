7a3959ee3e26c0d9e9a5dddabeeea5c3b4f5d848
ottojoash <34677630+ottojoash@users.noreply.github.com>
2023-10-23 08:45:12 +0300

    body pharser

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,11 @@
 const express = require('express');
+const bodyParser = require('body-parser'); // Require body-parser
 const app = express();
 const port = process.env.PORT || 3000;
 const userRoutes = require('./routes/userRoutes');
 
-// Middleware for JSON parsing
-app.use(express.json());
+// Middleware for JSON parsing using body-parser
+app.use(bodyParser.json());
 
 // Use the user registration routes
 app.use('/api/users', userRoutes);
